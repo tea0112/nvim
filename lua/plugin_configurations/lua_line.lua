dc636960e6fa4bbe9037986565eb751fdd64e0d3
tea0112 <ducthaidev@gmail.com>
2023-09-23 10:58:58 +0700

    update

--- lua/plugin_configurations/lua_line.lua
+++ lua/plugin_configurations/lua_line.lua
@@ -3,6 +3,22 @@ local M = {}
 function M.setup()
     require("lualine").setup({
         options = { theme = "onedark" },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+        },
     })
 end
 
