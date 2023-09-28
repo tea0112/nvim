be161f53f3e369db7336b914458a56046ada85f0
tea0112 <ducthaidev@gmail.com>
2023-09-28 16:36:37 +0700

    update

--- lua/plugin_configurations/indent_blankline.lua
+++ lua/plugin_configurations/indent_blankline.lua
@@ -1,8 +1,18 @@
 local M = {}
 
 function M.setup()
+    vim.opt.list = true
+    vim.opt.listchars:append("eol:↴")
+    vim.opt.listchars:append("space:⋅")
+    vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+
     require("indent_blankline").setup({
-        -- space_char_blankline = " ",
+        space_char_blankline = " ",
         show_end_of_line = true,
         -- for example, context is off by default, use this to turn it on
         show_current_context = true,
