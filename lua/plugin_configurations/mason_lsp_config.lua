0ecf1cb63167d95757618c0c79e6891e757161b0
tea0112 <ducthaidev@gmail.com>
2023-10-01 08:28:05 +0700

    update

--- lua/plugin_configurations/mason_lsp_config.lua
+++ lua/plugin_configurations/mason_lsp_config.lua
@@ -2,7 +2,15 @@ local M = {}
 
 function M.setup()
     require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer", "marksman", "jdtls", "pyright", "bashls", "clangd" },
+        ensure_installed = {
+            "lua_ls",
+            "rust_analyzer",
+            "marksman",
+            "jdtls",
+            "pyright",
+            "bashls",
+            "clangd",
+        },
     })
 end
 
