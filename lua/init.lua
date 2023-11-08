5393325a76e0149a20fc5794ba42eb61dfa60f80
tea0112 <ducthaidev@gmail.com>
2023-11-08 21:48:47 +0700

    update

--- lua/init.lua
+++ lua/init.lua
@@ -71,8 +71,8 @@ require("lazy").setup({
 -- for nvim tree --
 -------------------
 -- disable netrw at the very start of your init.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 ------------------
 -- plugin setup --
