9e483c098680579791dee6933f159e415e18d734
tea0112 <ducthaidev@gmail.com>
2024-02-01 16:05:46 +0700

    update

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,10 @@
 return {
     "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
     end,
 }
