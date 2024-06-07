8700f14942fc0e46e08658eec06014d9037e646f
tea0112 <ducthaidev@gmail.com>
2024-06-07 09:08:14 +0700

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,11 +2,11 @@ return {
     "stevearc/conform.nvim",
     config = function()
         require("conform").setup({
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
+            -- format_on_save = {
+            --     -- These options will be passed to conform.format()
+            --     timeout_ms = 500,
+            --     lsp_fallback = true,
+            -- },
             formatters_by_ft = {
                 lua = { "stylua" },
                 sh = { "shfmt" },
