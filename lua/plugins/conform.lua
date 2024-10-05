06e847c95ac6da85dd7994e5aaed2621bd592938
tea0112 <ducthaidev@gmail.com>
2024-10-05 09:59:11 +0700

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,6 +10,7 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 sh = { "shfmt" },
+                go = { "gofmt", "goimports" },
                 -- Conform will run multiple formatters sequentially
                 -- python = { "isort", "black" },
                 -- Use a sub-list to run only the first available formatter
