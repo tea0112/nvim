94f35497bc54a05564af1c8c09b2f7884419490d
tea0112 <ducthaidev@gmail.com>
2025-06-30 08:17:06 +0700

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,6 +13,7 @@ return {
                 sh = { "shfmt" },
                 go = { "gofmt", "goimports" },
                 json = { "jq" },
+                yaml = { "yamlfmt" },
                 -- Conform will run multiple formatters sequentially
                 -- python = { "isort", "black" },
                 -- Use a sub-list to run only the first available formatter
